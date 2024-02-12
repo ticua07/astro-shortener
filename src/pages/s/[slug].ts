44982ee8a9d8c4d4947ad58192c2013c2619a884
Ticua <ticua@ticua.ar>
2024-02-12 19:32:54 -0300

    slug redirection working

---
+++ src/pages/s/[slug].ts
@@ -0,0 +1,17 @@
+import type { APIRoute } from "astro";
+import { prisma } from "../../utils/prisma";
+import { getSession } from "auth-astro/server";
+import { z } from "astro/zod";
+
+export const GET: APIRoute = async ({ params, url, redirect }) => {
+
+    const { slug } = params;
+
+    const data = await prisma.link.findFirst({
+        where: {
+            slug
+        }
+    })
+
+    return redirect(data?.url || url.origin, 301)
+}
