7e853716c9e5dcd85b3dd6ebd92c4d2c52785c6b
Ticua <ticua@ticua.ar>
2024-02-12 10:04:48 -0300

    setup authjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+import auth from "auth-astro";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), svelte(), auth()],
+  output: "server",
+  adapter: vercel(),
+});
