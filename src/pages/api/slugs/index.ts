cbc70a88575d184c335cdee758a59f9984324a66
Ticua <ticua@ticua.ar>
2024-02-13 17:54:31 -0300

    add logging to api/slugs

--- src/pages/api/slugs/index.ts
+++ src/pages/api/slugs/index.ts
@@ -2,17 +2,22 @@ import type { APIRoute } from "astro";
 import { getSession } from "auth-astro/server";
 import { prisma } from "../../../utils/prisma"
 
-;
+    ;
 
 export const GET: APIRoute = async ({ request }) => {
-    const session = await getSession(request);
+    const session = await getSession(request).catch((err) => {
+        console.log(err)
+    });
+
     if (!session || !session.user?.email) { return new Response(JSON.stringify({ success: false })) }
 
     const data = await prisma.link.findMany({
         where: {
             creator: session?.user?.email
         }
-    })
+    }).catch((err) => {
+        console.log(err)
+    });
 
     return new Response(JSON.stringify(data))
 }
