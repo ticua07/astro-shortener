7b34cf95543d268a06aad698d0c972e2170140f4
Ticua <ticua@ticua.ar>
2024-02-13 16:36:09 -0300

    remove unused vite dependency

--- auth.config.js
+++ auth.config.js
@@ -1,17 +1,17 @@
 import GitHub from "@auth/core/providers/github";
 import { defineConfig } from "auth-astro";
-import { loadEnv } from "vite";
+// import { loadEnv } from "vite";
 
 // using import.meta.env also works
 // but loading the env here is done so that prisma has the env vars too
 // which for some reason aren't being loaded
-const env = loadEnv("production", process.cwd(), "");
+// const env = loadEnv("production", process.cwd(), "");
 
 export default defineConfig({
   providers: [
     GitHub({
-      clientId: env.GITHUB_CLIENT_ID,
-      clientSecret: env.GITHUB_CLIENT_SECRET,
+      clientId: import.meta.env.GITHUB_CLIENT_ID,
+      clientSecret: import.meta.env.GITHUB_CLIENT_SECRET,
     }),
   ],
 });
