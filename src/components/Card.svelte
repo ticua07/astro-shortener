dde2e6fe3854743e71b11d2bb3457efe0ff36afc
Ticua <ticua@ticua.ar>
2024-02-14 20:29:13 -0300

    Add copy functionality to Card

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -2,21 +2,31 @@
   import type { Link } from "../utils/types";
   import EditIcon from "../components/links/EditIcon.svelte";
   import TrashIcon from "../components/links/TrashIcon.svelte";
+  import CopyIcon from "../components/links/CopyIcon.svelte";
 
   export let data: Link;
   export let deleteItem: (slug: string) => Promise<void>;
+
+  const copyLinkToClipboard = () => {
+    navigator.clipboard.writeText(`https://link.ticua.ar/s/${data.slug}`);
+  };
 </script>
 
 <article
   class="border w-full flex flex-row gap-1 justify-center p-4 px-4 rounded border-zinc-700"
 >
   <div class="w-full">
-    <a
-      href="/s/{data.slug}"
-      class="flex flex-row gap-2 items-center line-clamp-1 text-xl leading-snug"
-    >
-      /s/{data.slug}
-    </a>
+    <div class="flex items-center gap-2">
+      <a href="/s/{data.slug}" class="line-clamp-1 text-xl leading-snug w-min">
+        /s/{data.slug}
+      </a>
+      <button
+        on:click={copyLinkToClipboard}
+        class="active:scale-90 transition-transform duration-100"
+      >
+        <CopyIcon />
+      </button>
+    </div>
     <p class="line-clamp-1 text-lg text-zinc-500">
       {data.url}
     </p>
