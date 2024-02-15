1abb5d829daf615c3a54a3c371263831808f8867
Ticua <ticua@ticua.ar>
2024-02-14 21:40:44 -0300

    make dashboard prettier on mobile

--- src/components/Links.svelte
+++ src/components/Links.svelte
@@ -25,7 +25,7 @@
 
 {#if !loading}
   <section
-    class="text-white mx-auto sm:w-[40rem] lg:w-3/4 px-2 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2"
+    class="text-white justify-center sm:w-[40rem] lg:w-3/4 sm:px-2 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2"
   >
     {#each data as short}
       <Card {deleteItem} data={short} />
@@ -33,5 +33,7 @@
   </section>
 {:else}
   <!-- <h1 class="text-3xl text-white">Loading</h1> -->
-  <Spinner />
+  <section class="flex justify-center">
+    <Spinner />
+  </section>
 {/if}
