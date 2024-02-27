94a10ce72cf95b32860deb1d66a7892928d2bdf5
Ticua <ticua@ticua.ar>
2024-02-27 19:24:27 -0300

    Block when loading font to fix font layout shift

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,6 +10,7 @@ export default {
       },
     },
     fontFamily: {
+      "sans": ["ui-sans-serif", "system-ui", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
       "roboto": "Roboto"
     }
   },
