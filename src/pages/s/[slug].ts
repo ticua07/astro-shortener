46dabd440cd86cedab68fec2592cde4d4920b241
Ticua <ticua@ticua.ar>
2024-12-05 21:34:39 -0300

    Remove redis as my upstash account is no longer working

--- src/pages/s/[slug].ts
+++ src/pages/s/[slug].ts
@@ -1,24 +1,10 @@
 import type { APIRoute } from "astro";
 import { prisma } from "../../utils/prisma";
-import { redis } from "../../utils/redis";
 
 
 export const GET: APIRoute = async ({ params, url, redirect }) => {
     const { slug } = params;
     if (!slug) redirect(url.origin)
-
-    const cache = await redis.get(slug!)
-
-    if (cache !== null) {
-        // Prevent the url from infinite requests
-        // by not allowing the url pointed to be the same as the url used to redirect
-        if (cache.startsWith(url.origin)) {
-            return redirect(url.origin)
-        } else {
-            console.log("CACHE HIT!")
-            return redirect(cache)
-        }
-    }
 
     const data = await prisma.link.findFirst({
         where: {
@@ -27,12 +13,6 @@ export const GET: APIRoute = async ({ params, url, redirect }) => {
     })
 
     if (data) {
-        await redis.set(slug!, data?.url, {
-            "EX": 30
-        })
-
-        console.log("CACHE SET!")
-
         // Prevent the url from infinite requests
         // by not allowing the url pointed to be the same as the url used to redirect
         if (data.url.startsWith(url.origin)) {
--- src/utils/redis.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "redis";
-
-const redis = createClient({
-    url: import.meta.env.REDIS_URL
-})
-
-await redis.connect()
-
-export { redis };
